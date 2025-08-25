d0776d539c4f249de668017e338be3db09052dff
Sota <breeze.prunus0707@gmail.com>
2025-08-25 16:59:18 +0900

    OpenFaceコマンド実行処理の実装

---
+++ modules/PyOpenFace.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def get_OpenFace_result() :
+    # PowerShell スクリプト実行
+    ps_cmd = ["powershell", "-ExecutionPolicy", "ByPass", "-File", "Exec_FeatureExtraction.ps1"]
+    result = subprocess.run(ps_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("PowerShell stdout:", result.stdout)
+    print("PowerShell stderr:", result.stderr)
