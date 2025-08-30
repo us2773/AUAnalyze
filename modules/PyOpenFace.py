b7dcc64a5443bad023f28b9e3d30870486b9cee3
us2773 <breeze.prunus0707@gmail.com>
2025-08-30 21:07:02 +0900

    input内の動画名を取得する関数を実装 （拡張子なし）

--- modules/PyOpenFace.py
+++ modules/PyOpenFace.py
@@ -1,4 +1,18 @@
 import subprocess
+import glob
+import os
+
+inputdir = "input"
+donedir = "done"
+def get_movie_file():
+    # inputディレクトリの動画ファイルのリストを返す
+    inputs = glob.glob(inputdir+"/*")
+    movie_list = []
+    for movie_path in inputs :
+        movie_name = os.path.splitext(os.path.basename(movie_path))[0]
+        movie_list.append(movie_name)
+    print(movie_list)
+    return movie_list
 
 def get_OpenFace_result() :
     # PowerShell スクリプト実行
@@ -8,4 +22,5 @@ def get_OpenFace_result() :
     print("PowerShell stderr:", result.stderr)
     
 if __name__ == "__main__":
-    get_OpenFace_result()
+    get_movie_file()
+    #get_OpenFace_result()
--- modules/data_regiseter.py
+++ modules/data_regiseter.py
@@ -5,6 +5,7 @@ import db_config
 def register() :
     # Engine作成
     engine = create_engine(f"postgresql+psycopg2://{db_config.user_name}:{db_config.password}@{db_config.host_name}/facehealthdb",echo=True)
+    # Table 作成
     Base.metadata.create_all(engine)
     for column in main_table.__table__.columns:
         print(column.name, column.type)
