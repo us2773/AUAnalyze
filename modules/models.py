1a1a19ff55176bb9edd507c7eb9e06d7694914ee
us2773 <breeze.prunus0707@gmail.com>
2025-08-31 12:56:48 +0900

    AU統計情報を格納するテーブル作成

--- modules/models.py
+++ modules/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase, mapped_column
 import uuid
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalchemy import Text, Uuid, DateTime, Float
+from sqlalchemy import Text, Uuid, DateTime, Integer, Float, ForeignKey
 
 class Base(DeclarativeBase):
     pass
@@ -10,6 +10,19 @@ class main_table(Base) :
     __tablename__ = "main_table"
     id = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
     movie_name = mapped_column(Text, unique=True)
-    date = mapped_column(DateTime)
+    date = mapped_column(DateTime(timezone=True))
     # fatigue_level = mapped_column(ARRAY(Float))
+    registed_date = mapped_column(DateTime)
     person = mapped_column(Text)
+    
+class au_table(Base) :
+    __tablename__ = "AU_table"
+    indiv_id = mapped_column(Uuid, primary_key=True, default=uuid.uuid4)
+    data_id = mapped_column(ForeignKey("main_table.id"), nullable=False)
+    trend_mean = mapped_column(ARRAY(Float))
+    trend_sd = mapped_column(ARRAY(Float))
+    noise_mean = mapped_column(ARRAY(Float))
+    noise_sd = mapped_column(ARRAY(Float))
+    num_of_peak = mapped_column(ARRAY(Integer))
+    peak_freq = mapped_column(ARRAY(Float))
+    
