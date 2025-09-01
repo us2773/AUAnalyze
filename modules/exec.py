6677e09576a88563feacf2da381b49ad8b7a2d67
us2773 <breeze.prunus0707@gmail.com>
2025-09-01 11:06:52 +0900

    #3 Main_table登録処理 （日付、撮影者はサンプルデータ）

--- modules/exec.py
+++ modules/exec.py
@@ -4,18 +4,19 @@ import data_regiseter
 
 outputdir = "output"
 
-# Input内の動画ファイル名取得
+# Input内の動画ファイル名取得(拡張子なし)
 input_movies = get_movie_file()
+print(input_movies)
 
 # movie to CSV
-get_OpenFace_result()
+# get_OpenFace_result()
 #transfer_input_movies() # デバッグ時はコメントアウト
 
-list = []
+df_list = []
 
 for file in input_movies :
     csv_file = f"{outputdir}/{file}.csv"
     df = csv_to_dataframe(csv_file)
-    list.append(df)
+    df_list.append(df)
 
-data_regiseter.register()
+data_regiseter.register(input_movies)
